23d4498c3c52f1ad0b48a0788e532bbed762ab85
ManarShahen <manarshahen2001.gmail.com>
2022-10-30 07:08:24 +0200

    update files

--- js/comic_details.js
+++ js/comic_details.js
@@ -9,21 +9,25 @@ var loader = document.getElementById("loader");
 
 
 function onloadFun(){
-    
     getComicDetailsFromServer("65091"); 
- };
+};
+
+
 const getComicDetailsFromServer = async (conmicId) => {
 
     const response = await fetch("https://gateway.marvel.com/v1/public/comics/"+conmicId+"?ts=1&apikey=a3fe408bde8bdcc10e4b86e2f8a692b5&hash=89a517a9bead0878ae6a0322fad7ff59&fbclid=IwAR2IKWDZ8tj5gq5zxb-Lgi0oZh6bcHrcPG33bpd8w6c8I7MIIlI5PK9qexw");
     const myJson = await response.json(); //extract JSON from the http response
+     fillPageContent(myJson);
+     hideLoaderLayer();
+}
+
+function fillPageContent(myJson){
     comicName.innerText = myJson.data.results[0].title ; 
     if(myJson.data.results[0].images.length !==0){
         comicImage.src = myJson.data.results[0].images[0].path+"." + myJson.data.results[0].images[0].extension ; 
     }
     else
     comicImage.src = myJson.data.results[0].thumbnail[0].path+"." + myJson.data.results[0].thumbnail[0].extension ; 
-
-
     if (typeof str === 'string' && str !== '') {
         description.innerText = myJson.data.results[0].description ;
     }
@@ -36,6 +40,8 @@ const getComicDetailsFromServer = async (conmicId) => {
     penclier.innerText =myJson.data.results[0].creators.items[1].name ;
     coverArtist.innerText =myJson.data.results[0].creators.items[1].name ;
 
+}
+function hideLoaderLayer(){
     loader.style.zIndex = -9999 ; 
     loader.innerHTML="";
 
